a11cea3f6a73fcb18788d21c99acc46596bf5769
unknown <hungryhul@gmail.com>
2019-07-26 10:55:47 -0400

    Domain Fixes

--- js/main.js
+++ js/main.js
@@ -20,4 +20,31 @@ window.onload = function (){
 
     var reddit = c.getElementById("XMLID_443_");
     reddit.setAttribute("onclick", "window.open('https://www.reddit.com/user/SBUHacks', '_blank');");
+
+    // Lazy Loading Youtube
+
+    var youtube = document.querySelectorAll( ".youtube" );
+
+	for (var i = 0; i < youtube.length; i++) {
+
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube[ i ].appendChild( image );
+				}( i ) );
+
+				youtube[i].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+							iframe.setAttribute( "frameborder", "0" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );
+	};
 }
